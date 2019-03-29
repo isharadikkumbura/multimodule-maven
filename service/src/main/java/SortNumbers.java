b5e9fb2a4594f1402f20e2b495a508a10db6570b
Ishara Dikkumbura <ishara.asdk@gmail.com>
2019-03-29 17:13:47 +0530

    added plugins

--- service/src/main/java/SortNumbers.java
+++ service/src/main/java/SortNumbers.java
@@ -1,18 +1,27 @@
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class SortNumbers {
 
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter number of items to be sorted");
-        int numberOfItems = scan.nextInt();
+//        System.out.println("Enter number of items to be sorted");
+//        int numberOfItems = scan.nextInt();
 
         System.out.println("Enter Items to be sorted");
-        int itemArray[] = new int[numberOfItems];
-        for (int i = 0; i < numberOfItems; i++) {
-            itemArray[i] = scan.nextInt();
-        }
+       // int itemArray[] = new int[numberOfItems];
+//        for (int i = 0; i < numberOfItems; i++) {
+//            itemArray[i] = scan.nextInt();
+//        }
 
+        String numberString = scan.nextLine();
+        List<Integer> list = Stream.of(numberString.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        int[] itemArray  = list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
         System.out.println("Choose the Sorting algorithm > Type 1 for Selection, Type 2 for Insertion, Type 3 for Bubble");
         int typeNumber = scan.nextInt();
 
