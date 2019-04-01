935a62595b7792588cb8f4d19fad1fba05d155e8
Ishara Dikkumbura <ishara.asdk@gmail.com>
2019-04-01 09:23:16 +0530

    variable changed

--- core/src/main/java/BubbleSort.java
+++ core/src/main/java/BubbleSort.java
@@ -1,7 +1,7 @@
 public class BubbleSort extends Sort{
     public void sort( int arr[]){
         int n= arr.length;
-        int i, j, temp;
+        int i, j, temp2;
         boolean swapped;
         for (i = 0; i < n - 1; i++)
         {
@@ -10,16 +10,17 @@ public class BubbleSort extends Sort{
             {
                 if (arr[j] > arr[j + 1])
                 {
-                    // swap arr[j] and arr[j+1]
-                    temp = arr[j];
+
+		
+                    // swap arr[j] and arr[j+1]  efdedwefdefef
+                    temp2 = arr[j];
                     arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    arr[j + 1] = temp2;
                     swapped = true;
                 }
             }
 
-            // IF no two elements were
-            // swapped by inner loop, then break
+            
             if (swapped == false)
                 break;
 
